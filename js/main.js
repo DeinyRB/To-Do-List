867a47ca7383c837ff6ef46815c8e21bdf66ed7f
unknown <deinyplata.04@gmail.com>
2023-02-19 12:21:00 -0600

    codificación del main.js para funcionalidad de lista de quehaceres, que contiene constantes, creación de imput, llamar datos exiastentes, creacion de eventos de escucha y validación

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// Constantes iniciales 
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+/*crear la imput */
+const inputDescription = document.getElementById('todo-item');
+
+const itemsContainer = document.getElementById('todo-cont');
+
+// Llamar data si existe en cuando cargue la pagina
+Items.displayItems();
+
+
+// Creamos Funcionalidad //
+
+/*Escuchador de eventos por click */
+//Evento de escucha de la forma
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+// Evento de escucha main para ingresar elementos
+main.addEventListener('keypress', (e) => {
+  // Obtener los valores
+const description = inputDescription.value;
+
+// Validacion
+if(description == '') {
+  // vacia por ahora, para luego llamar la funciom validadora
+}
+
+  // Mandar los valores, 
+  /*al tocar enter va ir agregando lo ingresado)
+  agrego despues de enter, para que al precionar enter y no haya escito nada, no haga registro(creamos validacion)*/
+  if(e.key === 'Enter' && description != '') {
+    const item = new ToDoItem(false, description);
+
+    // Agregar un nuevo todo
+    Items.addItem(item);
+
+// Agregar el LocalStore
+LocalStorage.addItem(item);
+
+    // Limpiar la input
+    Items.clearInput();
+  }
+});
+
+// Evento de escucha, para remover elementos
+itemsContainer.addEventListener('click', (e) => {
+  // Remover Elemento
+  if(e.target.classList.contains('remove')){
+    Items.removeItem(e.target);
+
+    LocalStorage.removeItem(
+      e.target.parentElement.previousElementSibling.lastElementChild.textContent,
+    );
+  }
+});
